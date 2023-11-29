776cefa23e7caa5e2f10c43069dac45367a9ff6d
MohamedSayed <Mohamedsaiko9@gmail.com>
2023-11-29 09:45:26 +0100

    Initial Commit

---
+++ project2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  project2
+//
+//  Created by Mohamed Sayed on 29.11.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
